7949635b88f5ba98dcbe23d13d1f119b6be05f75
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2024-02-16 10:32:57 +0500

    COUNTER APP

---
+++ COUNTER/file.js
@@ -0,0 +1,11 @@
+// function Click(number){
+//     alert(number)
+
+// }
+
+var counter=0
+let Click = function(number){
+    counter +=number
+    document.getElementById('no').innerHTML=counter
+
+}
